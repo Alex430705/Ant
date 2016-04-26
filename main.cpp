74d161ec2086fc1f209392e0d81a1148ddcd93f7
Alex <Alex430705@mail.ru>
2016-04-26 16:58:57 +0200

    изменял феромон

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #define BETTA	1			// коэффициент эвристики
 #define T_MAX	100			// время жизни колонии
 #define M		20			// количество муравьев в колонии
-#define Q		100			// количество
+#define Q		100.0			// количество
 #define RHO		0.5			// коэффициент испарения феромона
 
 double Sum(int index, double tau[][5], double ny[][5], QVector<int> tabu); //Суммирует ню и тау в знаменателе вероятностей
@@ -67,95 +67,69 @@ int main(int argc, char *argv[])
     }
 
     cout << endl << "Probabilities" << endl;
-//    for (int count = 0; count < 200; ++count)
-//    {
-//        int i = 0; //откуда идем
-//        double tmp = 0.0;
-//        int n = 0;
-//        QSet<int> tabu;
-//        QVector<double> way;
-//        tabu.insert(i);
-//        while (tabu.size() < 5)
-//        {
-//            for (int j=0; j<5; ++j) //куда идем
-//            {
-//                if ( i != j && !tabu.contains(j))
-//                {
-//                    double sum = Sum(i, tau, ny, tabu); //знаменатель
-//                    P[i][j] = 100.0 * ( pow(ny[i][j], alpha) * pow(tau[i][j], beta) ) / sum; //вероятность перехода
-//                    tmp += P[i][j];
-//                    vec.push_back(tmp);
-//                }
-//                else
-//                {
-//                    P[i][j] = 0.0;
-//                    vec.push_back(P[i][j]);
-//                }
-//            }
-//            QTime midnight(0,0,0);
-//            qsrand(midnight.secsTo(QTime::currentTime()));
-//            int myP = qrand() % 100 + 1;
-//            QVector<double>::iterator iter = qUpperBound(vec.begin(), vec.end(), myP);
-
-//            n = iter - vec.begin();
-//            way.push_back(L[i][n]);
-//            i = n;
-//            tabu.insert(i);
-//            tmp = 0.0;
-//            vec.clear();
-//        }
-//        way.push_back(L[n][0]);
-//        int lengthWay = 0;
-//        vector<double> v = way.toStdVector();
-//        lengthWay = accumulate(v.begin(), v.end(), lengthWay);
-//        if (lengthWay < bestWay){
-//            bestWay = lengthWay;
-//            qDebug() << "Best way = " << bestWay;
-//        }
-//        //обновляем феромон
-//        tabu.clear();
-//        v.clear();
-//        way.clear();
-//    }
 
     QVector<int> tabu;
     vector<int> way;
     vector<double> v;
-    double tmp = 0.0;
-    int i=0;
-    tabu.append(i);
-    while(tabu.length() < 5)
+
+    for (int k=0; k <20; ++k) //цикл по муравьям
     {
-        for (int j=0; j<5; ++j)
+        int i=0;
+        tabu.append(i);
+        while(tabu.length() < 5) //посещенные вершины
         {
-            if (i!=j && !tabu.contains(j))
+            for (int j=0; j<5; ++j) //куда идем
             {
-                double z = Sum(i, tau, ny, tabu); //знаменатель
-                P[i][j] = 100.0 * ( pow(ny[i][j], ALPHA) * pow(tau[i][j], BETTA) ) / z; //вероятность перехода
-                v.push_back(P[i][j]);
+                if (i!=j && !tabu.contains(j))
+                {
+                    double z = Sum(i, tau, ny, tabu); //знаменатель
+                    P[i][j] = 100.0 * ( pow(tau[i][j], ALPHA) * pow(ny[i][j], BETTA) ) / z; //вероятность перехода
+                    v.push_back(P[i][j]);
+                }
+                else
+                {
+                    P[i][j] = 0.0;
+                    v.push_back(0.0);
+                }
             }
-            else
-            {
-                P[i][j] = 0.0;
-                v.push_back(0.0);
-            }
+            int myP = qrand() % 100 + 1;
+            partial_sum(v.begin(), v.end(), v.begin());
+            vector<double>::iterator iter = upper_bound(v.begin(),v.end(), myP);
+            int to = iter - v.begin();
+            way.push_back(L[i][to]);
+            tabu.append(to);
+            i = to;
+            v.clear();
         }
-        int myP = qrand() % 100 + 1;
-        partial_sum(v.begin(), v.end(), v.begin());
-        vector<double>::iterator iter = upper_bound(v.begin(),v.end(), myP);
-        int to = iter - v.begin();
-        way.push_back(L[i][to]);
-        tabu.append(to);
-        i = to;
-        v.clear();
-    }
-    tabu.append(tabu.at(0));
-    way.push_back(L[*(tabu.end()-2)][*(tabu.end()-1)]);
-    int sum = accumulate(way.begin(),way.end(), 0);
-    if (sum < bestWay || bestWay < 0)
-    {
-        bestWay = sum;
-        qDebug() << "best way = " << bestWay;
+        tabu.append(tabu.at(0));
+        way.push_back(L[*(tabu.end()-2)][*(tabu.end()-1)]);
+        int length = accumulate(way.begin(),way.end(), 0);
+        if (length < bestWay || bestWay < 0)
+        {
+            bestWay = length;
+            qDebug() << "best way = " << bestWay;
+        }
+        //остовляем феромон на пути муравья
+        for( int i = 0; i < tabu.count(); ++i)
+        {
+            tau[i][i+1] += Q/length;
+            tau[i+1][i] = tau[i][i+1];
+        }
+        for(int i=0; i<5; ++i)
+        {
+            for(int j=0; j<5; ++j)
+            {
+                cout << tau[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        //испаряем феромон
+        for (int i=0; i<5; ++i)
+            for (int j=0; j<5; ++j)
+               if (i != j) tau[i][j] *= (1-RHO);
+        way.clear();
+        tabu.clear();
     }
     return a.exec();
 }
@@ -166,10 +140,7 @@ double Sum(int index, double tau[][5], double ny[][5] , QVector<int> tabu)
     for (int j = 0; j<5; ++j )
     {
         if (index != j && !tabu.contains(j))
-            sum += (double)ny[index][j] * (double)tau[index][j];
+            sum += pow((double)tau[index][j],ALPHA) * pow((double)ny[index][j], BETTA);
     }
-
-
-
     return sum;
 }
